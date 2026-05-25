fe1b83cf374dd2403c6e6c74a44155889cc857be
junilhan77 <junilhan77@gmail.com>
2026-05-25 21:40:39 +0900

    260525

---
+++ python_basic/practice.py
@@ -0,0 +1,32 @@
+# target_number = 7
+
+# user_input_guess = input("숫자를 맞춰보세요! 1에서 10까지의 숫자를 입력하세요: ")
+# guess = int(user_input_guess)
+
+# while True:
+#     if guess == target_number:
+#         print("정답입니다.")
+#         break
+#     else:
+#         print("아쉽지만 틀렸어요. 다시 시도해보세요!")
+#         user_input_guess = input("숫자를 맞춰보세요! 1에서 10까지의 숫자를 입력하세요: ")
+#         guess = int(user_input_guess)
+
+
+# for _ in range(10):
+#     print("안녕")
+
+# for i in range(6):
+#     print("나는 지금", i, "번째 반복 중이에요!")
+
+# for i in range(1, 6):
+#     print(i)
+
+# total = 0
+
+# for i in range(1, 1001):
+#     total = total + i
+#     print(i, "까지 더한 값:", total)
+
+# print("최종합계:", total)
+
