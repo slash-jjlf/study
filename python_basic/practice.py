d7e50089a52b832406dbb05aa67bd567b70f616b
slash <slash@mac13.com>
2026-05-26 15:34:07 +0900

    260526

--- python_basic/practice.py
+++ python_basic/practice.py
@@ -30,3 +30,53 @@
 
 # print("최종합계:", total)
 
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers)
+
+# fruits = ["사과", "바나나", "오렌지", "복숭아"]
+# print(fruits[0])
+
+# numbers = [1, 2, 3, 4]
+# numbers.append(5)
+# print(numbers)
+# numbers.append(6)
+# print(numbers)
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# combined = list1 + list2
+# print(combined)
+
+# empty_list = []
+# print(empty_list)
+# for i in range(1, 6):
+#     empty_list.append(i)
+# print(empty_list)
+
+# range_list = list(range(0, 4))
+# print(range_list)
+
+# students = ["철수", "영희", "민수"]
+
+# for index, student in enumerate(students):
+#     print(index+1, "번째 학생: ", student)
+
+# numbers = [10, 20, 30, 40, 50]
+# total = 0
+
+# for num in numbers:
+#     total += num
+
+# print("리스트 총합: ", total)
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# even_total = 0
+
+# for num in numbers:
+#     if num % 2 == 0:
+#         print("짝수:", num)
+#         even_total += num
+
+# print("짝수의 합:", even_total)
+
+
