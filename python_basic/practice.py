59270d739c76744bf68aad39cd433fef8d38606c
junilhan77 <junilhan77@gmail.com>
2026-05-26 18:28:43 +0900

    260526-a

--- python_basic/practice.py
+++ python_basic/practice.py
@@ -1,82 +1,15 @@
-# target_number = 7
+# user_input_score = input("점수를 쉼표로 구분하여 입력하세요: ")
+# score_list = user_input_score.split(",")
 
-# user_input_guess = input("숫자를 맞춰보세요! 1에서 10까지의 숫자를 입력하세요: ")
-# guess = int(user_input_guess)
+# print(score_list)
 
-# while True:
-#     if guess == target_number:
-#         print("정답입니다.")
-#         break
-#     else:
-#         print("아쉽지만 틀렸어요. 다시 시도해보세요!")
-#         user_input_guess = input("숫자를 맞춰보세요! 1에서 10까지의 숫자를 입력하세요: ")
-#         guess = int(user_input_guess)
+# total=0
 
+# for score in score_list:
+#     total += int(score)
 
-# for _ in range(10):
-#     print("안녕")
+# avg = total/len(score_list)
+# print("입력한 점수들의 평균은: ", avg)
 
-# for i in range(6):
-#     print("나는 지금", i, "번째 반복 중이에요!")
-
-# for i in range(1, 6):
-#     print(i)
-
-# total = 0
-
-# for i in range(1, 1001):
-#     total = total + i
-#     print(i, "까지 더한 값:", total)
-
-# print("최종합계:", total)
-
-# numbers = [1, 2, 3, 4, 5]
-# print(numbers)
-
-# fruits = ["사과", "바나나", "오렌지", "복숭아"]
-# print(fruits[0])
-
-# numbers = [1, 2, 3, 4]
-# numbers.append(5)
-# print(numbers)
-# numbers.append(6)
-# print(numbers)
-
-# list1 = [1, 2, 3]
-# list2 = [4, 5, 6]
-# combined = list1 + list2
-# print(combined)
-
-# empty_list = []
-# print(empty_list)
-# for i in range(1, 6):
-#     empty_list.append(i)
-# print(empty_list)
-
-# range_list = list(range(0, 4))
-# print(range_list)
-
-# students = ["철수", "영희", "민수"]
-
-# for index, student in enumerate(students):
-#     print(index+1, "번째 학생: ", student)
-
-# numbers = [10, 20, 30, 40, 50]
-# total = 0
-
-# for num in numbers:
-#     total += num
-
-# print("리스트 총합: ", total)
-
-# numbers = [1, 2, 3, 4, 5, 6]
-# even_total = 0
-
-# for num in numbers:
-#     if num % 2 == 0:
-#         print("짝수:", num)
-#         even_total += num
-
-# print("짝수의 합:", even_total)
 
 
