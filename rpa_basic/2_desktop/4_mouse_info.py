9a27925ac274772f0d5bd53a75ac6c4c2b33adaa
slash <slash@win11.ecom>
2026-07-22 16:46:00 +0900

    screenshot

---
+++ rpa_basic/2_desktop/4_mouse_info.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+# pyautogui.mouseInfo() # 마우스 정보 
+
+# 마우스 속도 조절
+pyautogui.PAUSE = 1
+
+pyautogui.sleep(2)
+
+for i in range(10):
+    pyautogui.move(100, 100)
+
+
+
+
+
+
+
---
+++ rpa_basic/2_desktop/5_screen.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# img = pyautogui.screenshot() # 스크린샷 찍기
+# img.save("screenshot.png") # 저장
+
+pyautogui.mouseInfo()
+
