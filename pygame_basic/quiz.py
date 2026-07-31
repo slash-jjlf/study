970e9ea85d713518356683492cebba8f245357b6
junil <junil@win11.com>
2026-07-31 22:13:45 +0900

    quiz

--- pygame_basic/quiz.py
+++ pygame_basic/quiz.py
@@ -19,6 +19,7 @@ import pygame
 ################################################################################################################################
 # 기본 초기화 (반드시 해야하는 것들)
 pygame.init()
+import random
 
 # 화면 크기 설정
 screen_width = 480 # 가로 크기
@@ -37,7 +38,7 @@ clock = pygame.time.Clock()
 background = pygame.image.load("background.png")
 
 # 캐릭터 만들기
-character = pygame.image.load("character.png")
+character = pygame.image.load("character1.png")
 character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
@@ -45,7 +46,17 @@ character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = screen_height - character_height
 
 # 이동 위치
+to_x = 0
+character_speed = 10
 
+# 똥만들기
+ddong = pygame.image.load("enermy1.png")
+ddong_size = ddong.get_rect().size
+ddong_width = ddong_size[0]
+ddong_height = ddong_size[1]
+ddong_x_pos = random.randint(0, screen_width-ddong_width)
+ddong_y_pos = 0
+ddong_speed = 10
 
 running = True # 게임이 진행중인가?
 while running:
@@ -55,14 +66,48 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                to_x += character_speed
+
+        if event.type == pygame.KEYUP and (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
+            to_x = 0
         
     # 3. 게임 캐릭터 위치 정의
+    character_x_pos += to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 똥의 위치 리셋
+    ddong_y_pos += ddong_speed
+    # 똥의 위치 리셋
+    if ddong_y_pos > screen_height:
+        ddong_y_pos = 0
+        ddong_x_pos = random.randint(0, screen_width - ddong_width)
 
     # 4. 충돌 처리
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    ddong_rect = ddong.get_rect()
+    ddong_rect.left = ddong_x_pos
+    ddong_rect.top = ddong_y_pos
+
+    if character_rect.colliderect(ddong_rect):
+        print("충돌했어요...")
+        running = False
 
     # 5. 화면에 그리기
     screen.blit(background, (0,0))
     screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(ddong, (ddong_x_pos, ddong_y_pos))
     
     pygame.display.update()
 
