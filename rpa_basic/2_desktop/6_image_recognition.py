83872168cf5680a7a013f84adff31b8d49c70ceb
slash <slash@win11.ecom>
2026-08-04 16:55:40 +0900

    pyautogui

--- rpa_basic/2_desktop/6_image_recognition.py
+++ rpa_basic/2_desktop/6_image_recognition.py
@@ -18,8 +18,48 @@ import pyautogui
 
 # for i in pyautogui.locateAllOnScreen("checkbox.png"):
 #     print(i)
-#     pyautogui.click(i)
+#     pyautogui.click(i, duration = 0.25)
 
-# locateAll은 모든 이미지 vs On은 처음 찾는 이미지만
-checkbox = pyautogui.locateOnScreen("checkbox.png")
-pyautogui.click(checkbox)
+# trash_can = pyautogui.locateOnScreen("trash_can.png")
+# pyautogui.moveTo(trash_can, duration = 0.25)
+
+# 속도 개선
+# 1. Grayscale
+# trash_can = pyautogui.locateOnScreen("trash_can.png", grayscale=True)
+# pyautogui.moveTo(trash_can)
+
+# 2. 범위 지정
+# trash_can = pyautogui.locateOnScreen("trash_can.png", region=(x, y, width, height))
+# pyautogui.moveTo(trash_can)
+
+# 3. 정확도 조정
+# run_btn = pyautogui.locateOnScreen("run_btn.png", confidence=0.7) # 컨피던스는 어느정도 비슷한지 척도 0.9999는 99% 비슷한 것
+# pyautogui.moveTo(run_btn)
+
+# 자동화 대상이 바로 보여지지 않는 경우
+# 1. 계속 기다리기
+# file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+# while file_menu_notepad is None:
+#     file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+#     print("발견 실패")
+
+# pyautogui.click(file_menu_notepad)
+
+# # 2. 일정 시간동안 기다리기 (time out)
+# import time
+# import sys
+
+# # timeout = 10 # 10초 대기
+# # start = time.time() # 시작 시간 설정
+# # file_menu_notepad = None
+# # while file_menu_notepad is None:
+# #     file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+# #     end = time.time() # 종료 시간 설정
+# #     if end - start > timeout: # 지정한 10초를 초과하면
+# #         print("시간 종료")
+# #         sys.exit()
+
+# pyautogui.click(file_menu_notepad)
+
+
+
