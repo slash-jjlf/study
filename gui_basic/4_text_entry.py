92e75db8e30a847126b6d5f1e8b22642fca67be5
slash <slash@mac.com>
2026-08-08 15:22:48 +0900

    gui_basic

---
+++ gui_basic/4_text_entry.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+root.title("J_Gui")
+root.geometry("640x480")
+
+txt = Text(root, width=30, height=5)
+txt.pack()
+txt.insert(END, "글자를 입력하세요")
+
+e = Entry(root, width=30) # text는 여러줄, entry는 한줄 입력할 때
+e.pack()
+e.insert(0, "한줄만 입력하세요")
+
+def btncmd():
+    # 내용 출력
+    print(txt.get("1.0", END)) # 1은 첫번째 라인, 0은 첫번째 column
+    print(e.get())
+
+    # 내용 삭제
+    txt.delete("1.0", END)
+    e.delete(0, END)
+
+btn = Button(root, text="클릭", command=btncmd)
+btn.pack()
+
+root.mainloop()
