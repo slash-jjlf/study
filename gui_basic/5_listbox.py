33b79b19f6a1ec7bd1febf58f6690b00f71eab81
junil <junil@win11.com>
2026-08-11 20:24:04 +0900

    gui_basic: checkbox

--- gui_basic/5_listbox.py
+++ gui_basic/5_listbox.py
@@ -13,11 +13,20 @@ listbox.insert(END, "포도")
 listbox.pack()
 
 def btncmd():
-    pass
+    # 삭제
+    # listbox.delete(0) # 맨 앞의 항목을 삭제
+
+    # 갯수 확인
+    # print("리스트에는", listbox.size(), "개가 있어요")
+
+    # 항목 확인(시작 idx, 끌 idx)
+    # print("1번째부터 3번째까지의 항목 : ", listbox.get(0, 2))
+
+    # 선택된 항목 확인(위치로 반환)
+    print("선택된 항목 : ", listbox.curselection())
 
 btn = Button(root, text="클릭", command=btncmd)
 btn.pack()
 
 root.mainloop()
-
 
