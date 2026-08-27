e801241a3342081ac4ad0bda37b7f2a7ff26ac2c
slash <slash@win11.ecom>
2026-08-27 16:55:20 +0900

    iframe

---
+++ rpa_basic/3_web/4_iframe.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Chrome()
+browser.get('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_radio')
+
+browser.switch_to.frame('iframeResult') # frame 전환
+
+elem = browser.find_element(By.XPATH, '//*[@id="html"]')
+elem.click()
+
+browser.switch_to.default_content() # 상위로 빠져나옴
+
+time.sleep(3)
+
+browser.quit()
