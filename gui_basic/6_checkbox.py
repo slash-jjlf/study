5f7361efee8064044af8de8d110bdade69ceb259
junilhan77 <junilhan77@gmail.com>
2026-08-30 22:02:20 +0900

    gui_basic_08302202

--- gui_basic/6_checkbox.py
+++ gui_basic/6_checkbox.py
@@ -1,25 +1,26 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.title("J_GUI")
 root.geometry("640x480")
 
-chkvar = IntVar() # chkvar에 int형으로 값을 저장한다
-chkbox = Checkbutton(root, text="오늘 하루 보지 않기", variable=chkvar)
+chkvar = tk.IntVar()  # chkvar에 int형으로 값을 저장한다
+chkbox = tk.Checkbutton(root, text="오늘 하루 보지 않기", variable=chkvar)
 # chkbox.select() # 자동 선택 처리
 # chkbox.deselect() # 선택 해제 처리
 chkbox.pack()
 
-chkvar2 = IntVar()
-chkbox2 = Checkbutton(root, text="일주일동안 보지 않기", variable=chkvar2)
+chkvar2 = tk.IntVar()
+chkbox2 = tk.Checkbutton(root, text="일주일동안 보지 않기", variable=chkvar2)
 chkbox2.pack()
 
+
 def btncmd():
-    print(chkvar.get()) # 0:체크해제, 1:체크
+    print(chkvar.get())  # 0:체크해제, 1:체크
     print(chkvar2.get())
 
-btn = Button(root, text="click", command=btncmd)
+
+btn = tk.Button(root, text="click", command=btncmd)
 btn.pack()
 
 root.mainloop()
-
--- gui_basic/7_radiobutton.py
+++ gui_basic/7_radiobutton.py
@@ -1,36 +1,43 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.title("J_GUI")
 root.geometry("640x480")
 
-Label(root, text="Select Menu").pack() # Int형으로 값을 저장
+tk.Label(root, text="Select Menu").pack()  # Int형으로 값을 저장
 
-burger_var = IntVar()
-btn_burger1 = Radiobutton(root, text="Basic Burger", value=1, variable=burger_var)
-btn_burger1.select() # 기본값
-btn_burger2 = Radiobutton(root, text="Cheese Burger", value=2, variable=burger_var)
-btn_burger3 = Radiobutton(root, text="Chicken Burger", value=3, variable=burger_var)
+burger_var = tk.IntVar()
+btn_burger1 = tk.Radiobutton(
+    root, text="Basic Burger", value=1, variable=burger_var)
+btn_burger1.select()  # 기본값
+btn_burger2 = tk.Radiobutton(
+    root, text="Cheese Burger", value=2, variable=burger_var)
+btn_burger3 = tk.Radiobutton(
+    root, text="Chicken Burger", value=3, variable=burger_var)
 
 btn_burger1.pack()
 btn_burger2.pack()
 btn_burger3.pack()
 
-Label(root, text="Seldect Drink").pack()
+tk.Label(root, text="Select Drink").pack()
 
-drink_var = StringVar() # str형으로 값을 저장
-btn_drink1 = Radiobutton(root, text="Coke", value="coke", variable=drink_var)
-btn_drink1.select() # 기본값
-btn_drink2 = Radiobutton(root, text="Sprite", value="sprite", variable=drink_var)
+drink_var = tk.StringVar()  # str형으로 값을 저장
+btn_drink1 = tk.Radiobutton(
+    root, text="Coke", value="coke", variable=drink_var)
+btn_drink1.select()  # 기본값
+btn_drink2 = tk.Radiobutton(
+    root, text="Sprite", value="sprite", variable=drink_var)
 
 btn_drink1.pack()
 btn_drink2.pack()
 
-def btncmd():
-    print(burger_var.get()) # 햄버거 중 선택된 라디오 항목의 값(value)을 출력
-    print(drink_var.get()) # 음료 중 선택된 라디오 항목의 값(value)을 출력
 
-btn = Button(root, text="click", command=btncmd)
+def btncmd():
+    print(burger_var.get())  # 햄버거 중 선택된 라디오 항목의 값(value)을 출력
+    print(drink_var.get())  # 음료 중 선택된 라디오 항목의 값(value)을 출력
+
+
+btn = tk.Button(root, text="click", command=btncmd)
 btn.pack()
 
 root.mainloop()
